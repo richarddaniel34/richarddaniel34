99488df88464a91a09e14ad8b1634e8285eb3971
Richard Daniel Moronta García <39407549+richarddaniel34@users.noreply.github.com>
2018-06-24 20:16:07 -0400

    Delete nombre.py

--- PYTHON/prac/nombre.py
+++
@@ -1,5 +0,0 @@
-print("Bienvenido")        #nombre en pantalla
-entrada = int(input("¿Cúantas veces quieres que se imprima mi nombre en pantalla?"))
-
-for i in range(0, entrada):
-    print("%d Yerison Yordalí Brito Cruz " % (i+1))
